5c7a21fbc793b9e3d997289e53be9c8141ad89b1
Duc Minh <minh98it@gmail>
2020-02-12 21:20:08 +0700

    Chapter_8: Basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
     length: {minimum: Settings.minimum_pass}
   before_save{self.email = email.downcase}
   has_secure_password
+
+  def self.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
     get  "/contact", to: "static_pages#contact"
     get  "/signup", to: "users#new"
     post  "/signup", to: "users#create"
-    resources :users, only: %i(show update destroy)
+    get    "/login",   to: "sessions#new"
+    post   "/login",   to: "sessions#create"
+    delete "/logout",  to: "sessions#destroy"
+    resources :users, only: %i(show update)
   end
 end
