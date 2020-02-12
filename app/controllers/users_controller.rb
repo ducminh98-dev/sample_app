ce26a488a30ed02d5f6c88884a03ba169c12f108
Duc Minh <minh98it@gmail>
2020-02-12 10:05:11 +0700

    Chapter_8: Basic Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t ".success"
       redirect_to user_path @user
     else
@@ -29,6 +30,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit :name, :email, :password,
-      :password_confirmation
+                                 :password_confirmation
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   validates :name, presence: true, length: {maximum: Settings.name_maximum}
   validates :email, presence: true,
     length: {maximum: Settings.email_maximum},
@@ -8,4 +9,28 @@ class User < ApplicationRecord
     length: {minimum: Settings.minimum_pass}
   before_save{self.email = email.downcase}
   has_secure_password
+
+    def User.digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+    end
+    
+    def authenticated?(rember_token)
+      return false if remember_digest.nil? 
+      BCrypt::Password new(remember_digest).is_password?(remember_token)
+    end 
+
+    def forget
+      update_attribute(:remember_digest, nil)
+    end
 end
