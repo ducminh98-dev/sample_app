7b3646582fb4029ec647ecb1a57f5ec8f50311e8
Duc Minh <minh98it@gmail>
2020-02-14 14:46:04 +0700

    Chap_10: Updating, showing, and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,91 @@
 class UsersController < ApplicationController
-  def new; end
+  before_action :logged_in_user, only: [:index,:edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def new
+    @user = User.new
+  end
+
+
+  def edit
+    @user = User.find_by id: params[:id]
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:alert] = t ".not_found"
+    redirect_to root_path
+  end
+
+
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      log_in @user
+      flash[:success] = t ".success"
+      redirect_to  @user
+    else
+      flash[:alert] = t".fails"
+      render :new
+    end
+  end
+
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = t ".del_succes"
+    redirect_to root_url
+  end
+
+
+  def update
+    @user =User.find_by id: params[:id]
+    if @user.update_attributes(user_params)
+      flash[:success] = t ".up_success"
+      redirect_to @user
+    else
+      flash[:alert] = t ".up_fail"
+      render :edit
+    end
+  end
+
+
+  def index
+   @users = User.paginate(page: params[:page])
+  end
+
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+                                 :password_confirmation
+  end
+
+  def logged_in_user
+    unless logged_in?
+       store_location
+      flash[:danger] = t ".pleas_lg"
+      redirect_to login_url
+    end
+  end
+
+
+  def correct_user
+  @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+  end
+
+
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   validates :name, presence: true, length: {maximum: Settings.name_maximum}
   validates :email, presence: true,
     length: {maximum: Settings.email_maximum},
@@ -6,6 +7,32 @@ class User < ApplicationRecord
         uniqueness: {case_sensitive: false}
   validates :password, presence: true,
     length: {minimum: Settings.minimum_pass}
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   before_save{self.email = email.downcase}
   has_secure_password
+
+
+    def User.digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def authenticated? remember_token
+      return false if remember_digest.nil?
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+      update_attribute(:remember_digest, nil)
+    end
 end
